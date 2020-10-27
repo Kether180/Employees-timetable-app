2cd1fbab788732a5ee07cabcae3c607b22b814b1
wilko180 <deutschland.alexander@gmail.com>
2020-10-27 12:07:51 +0100

    image commit

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express()
 
 const port = process.env.PORT || 3000 // is use for heroku 
 
+app.use(express.static('images'));
+
+app.get('/meditation', function (req, res) {  
+
+    const fileName = path.resolve(__dirname, 'images/meditation.jpg')
+    res.sendFile(fileName);
+})
+
 app.get('/', function(req, res) {
     res.send('go and have a drink!!!!!!')
 })
