ba50bf22e50f70f72ff29028a82f471f2c1162fc
wilko180 <deutschland.alexander@gmail.com>
2020-10-27 12:15:10 +0100

    3 commit change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000 // is use for heroku
 
 app.use(express.static('images'));
 
-app.get('/', function (req, res) {  
+app.get('/meditation', function (req, res) {  
 
     const fileName = path.resolve(__dirname, 'images/meditation.jpg')
     res.sendFile(fileName);
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 
 /*app.get('/', function(req, res) {
     res.send('go and have a drink!!!!!!')
-}) */
+})
 
 /*app.get('/about', function(req, res) {
     res.send('THIS IS THE ABOUT ENDPOINT')
