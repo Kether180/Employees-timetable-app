1a31d9b5480938f68c924f15e2264a9a01deb418
wilko180 <deutschland.alexander@gmail.com>
2020-10-27 12:12:35 +0100

    second try commit

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const port = process.env.PORT || 3000 // is use for heroku
 
 app.use(express.static('images'));
 
-app.get('/meditation', function (req, res) {  
+app.get('/', function (req, res) {  
 
     const fileName = path.resolve(__dirname, 'images/meditation.jpg')
     res.sendFile(fileName);
 })
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
     res.send('go and have a drink!!!!!!')
-})
+}) */
 
-app.get('/about', function(req, res) {
+/*app.get('/about', function(req, res) {
     res.send('THIS IS THE ABOUT ENDPOINT')
-})
+})*/
 
 app.listen(port, function() {
     console.log(`Running on port ${port}`)
