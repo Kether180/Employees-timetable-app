a41c894100783c2f2520bec0699533acdfd22a41
wilko180 <deutschland.alexander@gmail.com>
2020-10-27 11:41:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000 // is use for heroku 
+
+app.get('/', function(req, res) {
+    res.send('IT STILL WORKS!!!!!!')
+})
+
+app.get('/about', function(req, res) {
+    res.send('THIS IS THE ABOUT ENDPOINT')
+})
+
+app.listen(port, function() {
+    console.log(`Running on port ${port}`)
+})
